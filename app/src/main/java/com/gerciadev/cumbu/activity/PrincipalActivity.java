96ea298ecd914da3d67c590edec8ce10ad5a848b
gfonseca18 <gercia.fonseca@icloud.com>
2021-08-31 13:52:24 +0100

    Configurar a interface Homepage

--- app/src/main/java/com/gerciadev/cumbu/activity/PrincipalActivity.java
+++ app/src/main/java/com/gerciadev/cumbu/activity/PrincipalActivity.java
@@ -4,11 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.gerciadev.cumbu.databinding.ActivityPrincipalBinding;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -18,46 +19,47 @@ import androidx.navigation.ui.NavigationUI;
 //import com.gerciadev.cumbu.activity.databinding.ActivityPrincipalBinding;
 
 import com.gerciadev.cumbu.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityPrincipalBinding binding;
-
+    private MaterialCalendarView calendarView;
+    private TextView textoSaldo,textSaudacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        /*Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
 
-      /* binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        textoSaldo = findViewById(R.id.textSaldo);
+        textSaudacao = findViewById(R.id.textSaudacao);
+        calendarView = findViewById(R.id.calendarView);
 
-        setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
 
     public void adicionarReceita(View view){
         startActivity(new Intent(this,ReceitasActivity.class));
 
     }
     public void adicionarDespesa(View view){
-        startActivity(new Intent(this,DespesasActivity.class));
+        startActivity(new Intent(this, DespesasActivity.class));
+    }
+    public void configuracaoCalendarView(){
+        CharSequence meses [] = {"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outobro","Novembro","Dezembro"};
+        calendarView.setTitleMonths(meses);
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
     }
 }
